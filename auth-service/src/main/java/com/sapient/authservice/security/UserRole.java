15a5f3e8650ea8e7ba8f0e31b7d272e72a343726
Harsh Jaiswal <harshjaiswal253@gmail.com>
2019-06-17 12:44:49 +0530

    first commit

---
+++ auth-service/src/main/java/com/sapient/authservice/security/UserRole.java
@@ -0,0 +1,5 @@
+package com.sapient.authservice.security;
+
+public enum UserRole {
+    USER, ADMIN;
+}
---
+++ zuul/src/main/java/com/sapient/zuul/security/UserRole.java
@@ -0,0 +1,5 @@
+package com.sapient.zuul.security;
+
+public enum UserRole {
+    USER, ADMIN;
+}
